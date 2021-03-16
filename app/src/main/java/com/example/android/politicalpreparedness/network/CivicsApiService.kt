4338517a131fa24b60117330a659d13a64c107fe
Ioannis Anifantakis <ioannisanif@gmail.com>
2021-03-16 14:16:30 +0200

    Added HttpLoggingInterceptor

--- app/src/main/java/com/example/android/politicalpreparedness/network/CivicsApiService.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/network/CivicsApiService.kt
@@ -24,6 +24,8 @@ private val moshi = Moshi.Builder()
         .add(KotlinJsonAdapterFactory())
         .build()
 
+
+
 private val retrofit = Retrofit.Builder()
         .addConverterFactory(MoshiConverterFactory.create(moshi))
         .addCallAdapterFactory(CoroutineCallAdapterFactory())
--- app/src/main/java/com/example/android/politicalpreparedness/network/CivicsHttpClient.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/network/CivicsHttpClient.kt
@@ -3,25 +3,34 @@ package com.example.android.politicalpreparedness.network
 import android.util.Log
 import com.example.android.politicalpreparedness.BuildConfig
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import kotlin.math.log
+
 
 class CivicsHttpClient: OkHttpClient() {
 
     companion object {
 
-        private const val API_KEY = BuildConfig.API_KEY //TODO COMPLETED: Place your API Key Here
+        const val API_KEY =  BuildConfig.API_KEY //TODO COMPLETE: Place your API Key Here
+
 
         fun getClient(): OkHttpClient {
+            val logging = HttpLoggingInterceptor(object : HttpLoggingInterceptor.Logger {
+                override fun log(message: String) {
+                    Log.d("TEST", "HttpLoggingInterceptor: $message")
+                }
+            })
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+
             return Builder()
+                    .addInterceptor(logging)
                     .addInterceptor { chain ->
                         val original = chain.request()
                         val url = original
-                                .url()
+                                .url
                                 .newBuilder()
                                 .addQueryParameter("key", API_KEY)
                                 .build()
-
-                        Log.d("TEST", url.toString())
-
                         val request = original
                                 .newBuilder()
                                 .url(url)
